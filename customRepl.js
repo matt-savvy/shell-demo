71b8f2842223cc1d1bddd9e29eb362c245e1ef94
Matt Savoia <matt@1-800-rad-dude.com>
2022-12-06 14:44:08 -0500

    Add customRepl

---
+++ customRepl.js
@@ -0,0 +1,28 @@
+const repl = require("repl");
+const { connect } = require("./utils/connect");
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/shell-demo";
+const { User } = require("./models");
+
+(async () => {
+  console.log("connecting to database...");
+  try {
+    await connect(MONGO_URI);
+    console.log("connected");
+    startRepl();
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+// These will be globals for the repl
+const context = {
+  User,
+};
+
+function startRepl() {
+  const r = repl.start("> ");
+  for (let key in context) {
+    r.context[key] = context[key];
+  }
+}
